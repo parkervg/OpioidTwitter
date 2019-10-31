22b2c28d6d5a3f32e67e8e65314e2096068beab9
parkervg <44219290+parkervg@users.noreply.github.com>
2019-10-31 09:30:31 -0700

    Dropped index column

--- data/db_to_csv.py
+++ data/db_to_csv.py
@@ -3,4 +3,5 @@ import psycopg2
 conn = psycopg2.connect(dbname='opiods', user='parkerglenn', host='localhost')
 
 df = pd.read_sql("select * from Tweets", conn)
-df.to_csv("opioid_tweets.csv", encoding = "utf-8")
+
+df.to_csv("data/opioid_tweets.csv", encoding = "utf-8", index = False)
