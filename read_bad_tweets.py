8eeeb12d64b4a202300bb525029daa0aa6dd36e6
parkervg <parkervg5@gmail.com>
2019-11-05 16:28:58 -0800

    Added txt file of bad tweet ids

---
+++ read_bad_tweets.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv("data/opioid_tweets.csv")
+
+with open("bad_tweets.txt", "r") as f:
+    ids = [i.strip() for i in f.read().split(",")]
+
+for item in df[df["id"].isin(ids)].iterrows():
+    print()
+    print("ID: {}".format(item[1]["id"]))
+    print()
+    print(item[1]["content"])
+    print()
+    print("User: {}".format(item[1]["user_name"]))
+    print("_____________________________________________________________________________________________")
